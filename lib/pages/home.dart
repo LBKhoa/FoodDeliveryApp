22c255fd44d64c9056f96bfbaba495360b036770
LBKhoa <lebaokhoa.200329@gmail.com>
2024-12-10 15:16:02 +0700

    Creating a Home Screen Page UI from Scratch

---
+++ lib/pages/home.dart
@@ -0,0 +1,349 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:fooddeliveryapp/widget/widget_support.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  bool icecream = false, pizza = false, salad = false, burger = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          margin: const EdgeInsets.only(
+            top: 10.0,
+            left: 20.0,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                crossAxisAlignment:
+                    CrossAxisAlignment.center, // Đảm bảo các phần tử ngang hàng
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("Hello, Lê Bảo Khoa",
+                      style: AppWidget.boldTextFeildStyle()),
+                  Container(
+                    margin: const EdgeInsets.only(right: 20.0),
+                    padding: const EdgeInsets.all(8),
+                    decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: const Icon(
+                      Icons.shopping_cart,
+                      color: Colors.white,
+                      size: 24,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Text("Món Ăn Ngon", style: AppWidget.HeadlineTextFeildStyle()),
+              Text("Khám Phá Món Ngon", style: AppWidget.LightTextFeildStyle()),
+              SizedBox(
+                height: 20.0,
+              ),
+              Container(
+                  margin: EdgeInsets.only(right: 20.0), child: showItem()),
+              SizedBox(
+                height: 30.0,
+              ),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.all(4),
+                      child: Material(
+                        elevation: 5.0,
+                        borderRadius: BorderRadius.circular(20),
+                        child: Container(
+                          padding: EdgeInsets.all(14),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Image.asset(
+                                "images/salad2.png",
+                                height: 150,
+                                width: 150,
+                                fit: BoxFit.cover,
+                              ),
+                              Text(
+                                "Salad Rau Củ",
+                                style: AppWidget.semiBooldTextFeildStyle(),
+                              ),
+                              SizedBox(
+                                height: 5.0,
+                              ),
+                              Text(
+                                "Tươi và healthy",
+                                style: AppWidget.LightTextFeildStyle(),
+                              ),
+                              SizedBox(
+                                height: 5.0,
+                              ),
+                              Text(
+                                "50000 VNĐ",
+                                style: AppWidget.semiBooldTextFeildStyle(),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 15.0,
+                    ),
+                    Container(
+                      margin: EdgeInsets.all(4),
+                      child: Material(
+                        elevation: 5.0,
+                        borderRadius: BorderRadius.circular(20),
+                        child: Container(
+                          padding: EdgeInsets.all(14),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Image.asset(
+                                "images/salad3.png",
+                                height: 150,
+                                width: 150,
+                                fit: BoxFit.cover,
+                              ),
+                              Text(
+                                "Salad Trái Cây",
+                                style: AppWidget.semiBooldTextFeildStyle(),
+                              ),
+                              SizedBox(
+                                height: 5.0,
+                              ),
+                              Text(
+                                "Tươi và healthy",
+                                style: AppWidget.LightTextFeildStyle(),
+                              ),
+                              SizedBox(
+                                height: 5.0,
+                              ),
+                              Text(
+                                "55000 VNĐ",
+                                style: AppWidget.semiBooldTextFeildStyle(),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 15.0,
+                    ),
+                    Container(
+                      margin: EdgeInsets.all(4),
+                      child: Material(
+                        elevation: 5.0,
+                        borderRadius: BorderRadius.circular(20),
+                        child: Container(
+                          padding: EdgeInsets.all(14),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Image.asset(
+                                "images/salad4.png",
+                                height: 150,
+                                width: 150,
+                                fit: BoxFit.cover,
+                              ),
+                              Text(
+                                "Salad Ức Gà",
+                                style: AppWidget.semiBooldTextFeildStyle(),
+                              ),
+                              SizedBox(
+                                height: 5.0,
+                              ),
+                              Text(
+                                "Cay và Tươi",
+                                style: AppWidget.LightTextFeildStyle(),
+                              ),
+                              SizedBox(
+                                height: 5.0,
+                              ),
+                              Text(
+                                "68000 VNĐ",
+                                style: AppWidget.semiBooldTextFeildStyle(),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 30.0,
+              ),
+              Container(
+                margin: EdgeInsets.only(right: 20.0),
+                child: Material(
+                  elevation: 5.0,
+                  borderRadius: BorderRadius.circular(20),
+                  child: Container(
+                    padding: EdgeInsets.all(5),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Image.asset(
+                          "images/salad3.png",
+                          height: 120,
+                          width: 120,
+                          fit: BoxFit.cover,
+                        ),
+                        SizedBox(width: 20.0,),
+                        Column(children: [
+                          Container(
+                            width: MediaQuery.of(context).size.width/2,
+                              child: Text("Salad Đậu Gà Địa Trung Hải", style: AppWidget.semiBooldTextFeildStyle(),)),
+                          SizedBox(height: 5.0,),
+                          Container(
+                              width: MediaQuery.of(context).size.width/2,
+                              child: Text("Phô mai dê", style: AppWidget.LightTextFeildStyle(),)),
+                          SizedBox(height: 5.0,),
+                          Container(
+                              width: MediaQuery.of(context).size.width/2,
+                              child: Text("55000 VNĐ", style: AppWidget.semiBooldTextFeildStyle(),)),
+                        ],)
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget showItem() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        GestureDetector(
+          onTap: () {
+            icecream = true;
+            pizza = false;
+            salad = false;
+            burger = false;
+            setState(() {});
+          },
+          child: Material(
+            elevation: 5.0,
+            borderRadius: BorderRadius.circular(10),
+            child: Container(
+              decoration: BoxDecoration(
+                  color: icecream ? Colors.black : Colors.white,
+                  borderRadius: BorderRadius.circular(10)),
+              padding: const EdgeInsets.all(8),
+              child: Image.asset(
+                "images/ice-cream.png",
+                height: 40,
+                width: 40,
+                fit: BoxFit.cover,
+                color: icecream ? Colors.white : Colors.black,
+              ),
+            ),
+          ),
+        ),
+        GestureDetector(
+          onTap: () {
+            icecream = false;
+            pizza = true;
+            salad = false;
+            burger = false;
+            setState(() {});
+          },
+          child: Material(
+            elevation: 5.0,
+            borderRadius: BorderRadius.circular(10),
+            child: Container(
+              decoration: BoxDecoration(
+                  color: pizza ? Colors.black : Colors.white,
+                  borderRadius: BorderRadius.circular(10)),
+              padding: const EdgeInsets.all(8),
+              child: Image.asset(
+                "images/pizza.png",
+                height: 40,
+                width: 40,
+                fit: BoxFit.cover,
+                color: pizza ? Colors.white : Colors.black,
+              ),
+            ),
+          ),
+        ),
+        GestureDetector(
+          onTap: () {
+            icecream = false;
+            pizza = false;
+            salad = true;
+            burger = false;
+            setState(() {});
+          },
+          child: Material(
+            elevation: 5.0,
+            borderRadius: BorderRadius.circular(10),
+            child: Container(
+              decoration: BoxDecoration(
+                  color: salad ? Colors.black : Colors.white,
+                  borderRadius: BorderRadius.circular(10)),
+              padding: const EdgeInsets.all(8),
+              child: Image.asset(
+                "images/salad.png",
+                height: 40,
+                width: 40,
+                fit: BoxFit.cover,
+                color: salad ? Colors.white : Colors.black,
+              ),
+            ),
+          ),
+        ),
+        GestureDetector(
+          onTap: () {
+            icecream = false;
+            pizza = false;
+            salad = false;
+            burger = true;
+            setState(() {});
+          },
+          child: Material(
+            elevation: 5.0,
+            borderRadius: BorderRadius.circular(10),
+            child: Container(
+              decoration: BoxDecoration(
+                  color: burger ? Colors.black : Colors.white,
+                  borderRadius: BorderRadius.circular(10)),
+              padding: const EdgeInsets.all(8),
+              child: Image.asset(
+                "images/burger.png",
+                height: 40,
+                width: 40,
+                fit: BoxFit.cover,
+                color: burger ? Colors.white : Colors.black,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
