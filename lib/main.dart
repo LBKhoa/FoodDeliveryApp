3cf6c4b7cb4fc3b2f146fa3719d2d1d30359573b
LBKhoa <lebaokhoa.200329@gmail.com>
2024-12-15 17:28:18 +0700

    Adding a Firebase to our App, Creating a Function for SignUp Page and LogIn Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:fooddeliveryapp/pages/bottomnav.dart';
 import 'package:fooddeliveryapp/pages/home.dart';
@@ -5,7 +6,9 @@ import 'package:fooddeliveryapp/pages/login.dart';
 import 'package:fooddeliveryapp/pages/onboard.dart';
 import 'package:fooddeliveryapp/pages/signup.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
